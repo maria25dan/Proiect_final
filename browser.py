a9ec7ecd43533af5cc64e8e1f084dbceedbb7d4b
maria25dan <127100652+maria25dan@users.noreply.github.com>
2024-03-12 22:34:36 +0200

    Delete browser.py

--- browser.py
+++
@@ -1,7 +0,0 @@
-from seleniumbase import Driver
-
-
-class Browser:
-    def open_browser(self):
-        self.driver = Driver(browser="chrome", headless=False)
-        self.driver.maximize_window()
