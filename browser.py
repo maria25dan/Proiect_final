4e2c9ff0fcf06e2c2d5df4d510e91fcd2cb1123c
maria25dan <127100652+maria25dan@users.noreply.github.com>
2024-03-13 14:26:03 +0200

    update file

--- browser.py
+++ browser.py
@@ -2,5 +2,7 @@ from seleniumbase import Driver
 
 
 class Browser:
-    driver = Driver(browser="chrome", headless=False)
-    driver.maximize_window()
+    def open_browser(self):
+        driver = Driver(browser="chrome", headless=False)
+        driver.maximize_window()
+        return driver
